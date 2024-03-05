e8ae161365b068b86baacb194b900e77937c3251
taya reut <tasyareut@gmail.com>
2024-03-05 19:30:56 +0100

    first commit

---
+++ src/main/java/com/example/boxschoolservice/BoxSchoolServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.boxschoolservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxSchoolServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoxSchoolServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/boxschoolservice/BoxSchoolServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boxschoolservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxSchoolServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
